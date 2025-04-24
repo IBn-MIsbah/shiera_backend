a9b25f03b2345b94b73a800ce7fefbb96ef0aa27
IBn-MIsbah <160459932+IBn-MIsbah@users.noreply.github.com>
2025-04-24 21:42:28 +0300

    add vercel.json, use in-memory array to store data

--- daily-quote-app/server.js
+++ daily-quote-app/server.js
@@ -1,35 +1,32 @@
 import express, { response } from 'express';
-import axios from 'axios';
 
 
 const app = express();
 const port = 3000;
-const API_KEY = 'lrkk1P/bklOzHNzEYlQNfQ==zv7d3eGBlWCMmkab';
-const api_url = 'https://api.api-ninjas.com/v1/quotes'
 
 app.use(express.static('public'));
 app.set('view engine', 'ejs');
 
+const quotesList = [
+    { text: "Believe you can and you're halfway there.", author: "Theodore Roosevelt" },
+    { text: "Every day is a second chance.", author: "Oprah Winfrey" },
+    { text: "You can't stop the waves, but you can learn to surf", author: "Winston Churchill" },
+    { text: "Don't watch the clock; do what it does. Keep going.", author: "Sam Levenson" },
+    { text: "It always seems impossible until it's done.", author: "Nelson Mandela" },
+    { text: "Act as if what you do makes a difference. It does.", author: "William James" },
+    { text: "Talk is cheap. Show me the code", author: " Linus Torvalds" },
+    
+  ];
+
 app.get('/', async (req, res)=>{
 
-   try{
-        const response = await axios.get(api_url, {
-        headers:{
-          ' X-Api-Key': API_KEY
-        }
-    });
-
-    const quotesData = Array.isArray(response.data) ? response.data[0] : response.data
-    res.render('index', {
-        quote: quotesData.quote,
-        author: quotesData.author
-    })
-} catch(err){
-    console.error(err)
-}
+    const index = Math.floor(Math.random() * quotesList.length)
+    res.render('index', {quote: quotesList[index]})
 })
 
 app.listen(port, (err)=>{
     if(err) {throw err}
     console.log(`server active on port: ${port}`);
 })
+
+export default app;
